755d65abe44713da968f14a879ae44bbd2fe5e24
SolarWaterDev <86333848+vSolarWater@users.noreply.github.com>
2022-11-17 15:43:54 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CommandLineFun/games.hpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+void randomNumGame() {
+    // Not done
+    
+}
+
+void helloWorld() {
+  cout << "Hello world!";
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "games.hpp"
+
+using namespace std;
+
+int main() {
+    string game;
+    while (true) {
+        cout << "Enter game: "; cin >> game;
+
+        if (game == "hello") {
+          helloWorld();
+        }
+    }
+    
+}
